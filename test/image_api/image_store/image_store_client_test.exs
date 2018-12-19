50c2257c1beaac27be56679ea484b6d4cba88ed0
Aleksandar Dimov <isavitaisa@gmail.com>
2018-12-19 20:19:23 +0000

    Add tests for downloading and removing image from url

--- test/image_api/image_store/image_store_client_test.exs
+++ test/image_api/image_store/image_store_client_test.exs
@@ -3,10 +3,38 @@ defmodule ImageApi.ImageStore.ImageStoreClientTest do
   alias ImageApi.ImageStore.ImageStoreClient
 
   describe "download_image/1" do
-    test "returns file too big error when the file is bigger than 2MB"
+    test "downloads the file when is smaller than or equal to 2MB" do
+      url = "http://speedtest.ftp.otenet.gr/files/test100k.db"
+
+      assert {:ok, path} = ImageStoreClient.download_image(url)
+
+      ImageStoreClient.remove_image(path)
+    end
+
+    test "returns file too big error when the file is bigger than 2MB" do
+      url = "http://speedtest.ftp.otenet.gr/files/test10Mb.db"
+
+      assert {:error, :file_size_is_too_big} = ImageStoreClient.download_image(url)
+    end
   end
 
   describe "remove_image/1" do
-    test "removes the file and the directory in which an image is it"
+    setup do
+      directory = "/tmp/random_folder"
+      File.mkdir_p(directory)
+      image_path = directory <> "/random.jpg"
+      File.write!(image_path, "not real image")
+
+      {:ok, directory: directory, image_path: image_path}
+    end
+
+    test "removes the file and the directory of the image", %{
+      directory: directory,
+      image_path: image_path
+    } do
+      expected = {:ok, [directory, image_path]}
+
+      assert ImageStoreClient.remove_image(image_path) == expected
+    end
   end
 end
